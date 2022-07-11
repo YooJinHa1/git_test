93b2cd877e00b707c4cc71ffeb1d92794898ca87
YooJinHa1 <poop431@naver.com>
2022-07-11 10:02:56 +0900

    React Test

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import logo from './logo.svg';
-import styles from './App.scss';
-import classNames  from 'classnames/bind';
-const cx=classNames.bind(styles);
+//import logo from './logo.svg';
+//import styles from './App.scss';
+//import classNames  from 'classnames/bind';
+import Button  from './components/Button';
+//const cx=classNames.bind(styles);
 
 
 
 function App() {
-  const isBlue = true;
+
   return (
-    <div className={cx('box',{blue:isBlue})}>
-      <div className={cx('box-inside')}/>
+    <div>
+      <Button>버튼</Button>
     </div>
   );
 }
